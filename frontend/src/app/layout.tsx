db1902cc02a7fbc634346068ef8e15a1904d9c20
xzvw <128382145+xzvw@users.noreply.github.com>
2024-01-30 01:15:48 +0800

    Update layout

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,17 +1,20 @@
 import type { Metadata } from 'next'
+import type { ReactNode } from 'react'
 
 export const metadata: Metadata = {
   title: 'Uniqlo Sales Alert',
 }
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode
-}>) {
+export type RootLayoutProps = Readonly<{
+  children: ReactNode
+}>
+
+function RootLayout({ children }: RootLayoutProps) {
   return (
     <html>
       <body>{children}</body>
     </html>
   )
 }
+
+export default RootLayout
