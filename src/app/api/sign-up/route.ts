bda485f700643540f6b7c3c95ee1494cb94106a4
xzvw <128382145+xzvw@users.noreply.github.com>
2024-02-01 02:49:04 +0800

    Implement sign-up API

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -1,3 +1,13 @@
+import {
+  DynamoDBClient,
+  PutItemCommand,
+  QueryCommand,
+} from '@aws-sdk/client-dynamodb'
+import bcrypt from 'bcrypt'
+import { v4 as uuidv4 } from 'uuid'
+
+const TABLE_NAME = 'dev-users'
+
 export async function POST(request: Request) {
   const requestBody = await request.json()
 
@@ -14,6 +24,75 @@ export async function POST(request: Request) {
     )
   }
 
-  // @todo
-  return Response.json(requestBody)
+  const client = new DynamoDBClient({
+    region: process.env.AWS_REGION,
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+    },
+  })
+
+  try {
+    const emailExists = await client.send(
+      new QueryCommand({
+        TableName: TABLE_NAME,
+        IndexName: 'email-index',
+        KeyConditionExpression: 'email = :email',
+        ExpressionAttributeValues: {
+          ':email': { S: email },
+        },
+      })
+    )
+
+    if (typeof emailExists.Count !== 'number') {
+      return Response.json(
+        { message: 'Error registering user.' },
+        { status: 500 }
+      )
+    }
+
+    if (emailExists.Count > 0) {
+      return Response.json(
+        { message: 'Email already exists.' },
+        { status: 400 }
+      )
+    }
+  } catch (error) {
+    console.error(error)
+
+    return Response.json(
+      { message: 'Error registering user.' },
+      { status: 500 }
+    )
+  }
+
+  const userId = uuidv4()
+  const passwordHash = await bcrypt.hash(password, 10)
+
+  try {
+    await client.send(
+      new PutItemCommand({
+        TableName: TABLE_NAME,
+        Item: {
+          userId: { S: userId },
+          email: { S: email },
+          passwordHash: { S: passwordHash },
+          createdAt: { S: new Date().toISOString() },
+          updatedAt: { S: new Date().toISOString() },
+        },
+      })
+    )
+
+    return Response.json(
+      { message: 'User registered successfully.' },
+      { status: 201 }
+    )
+  } catch (error) {
+    console.error(error)
+
+    return Response.json(
+      { message: 'Error registering user.' },
+      { status: 500 }
+    )
+  }
 }
